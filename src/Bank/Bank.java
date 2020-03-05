06aab87d08b077e2a7c722c05af8039cb786bc57
Julian <cc7568mc@gmail.com>
2020-03-05 12:14:43 +0100

    Huiiii

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -3,4 +3,18 @@ package Bank;
 public class Bank {
 
     private Bank b;
+
+    private Bank()
+    {
+        while(true)
+        {
+            System.out.println("Huiii"):
+        }
+    }
+
+    public static Bank geben()
+    {
+        Bank();
+        return b;
+    }
 }
